1d5513a5c30917a76b879b0ac39a2756d95c5885
PedroSDD <psdd91@gmail.com>
2015-12-06 10:14:58 +0000

    Bit position

---
+++ scrubber.py
@@ -0,0 +1,13 @@
+#INPUT SAMPLE:
+# The first argument is a source strings and the characters that need to be scrubbed.
+
+def scrubber(sentence, letters):
+    new_sentence = list(sentence)
+
+    for letter in letters:
+        for i, char in enumerate(sentence):
+            if letter == char:
+                new_sentence[i]=""
+    print ''.join(new_sentence)
+
+scrubber("hello world", "def")
