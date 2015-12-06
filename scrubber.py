22cddaeeb63df625ab26f5a87246491a133f7b16
PedroSDD <psdd91@gmail.com>
2015-12-06 10:15:41 +0000

    Bit position

--- scrubber.py
+++ scrubber.py
@@ -2,7 +2,8 @@
 # The first argument is a source strings and the characters that need to be scrubbed.
 
 def scrubber(sentence, letters):
-    new_sentence = list(sentence)
+
+    new_sentence = list(sentence
 
     for letter in letters:
         for i, char in enumerate(sentence):
