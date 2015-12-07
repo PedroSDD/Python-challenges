f5cf19cf03ef636afe498d6394444fab0c24903b
PedroSDD <psdd91@gmail.com>
2015-12-07 23:36:24 +0000

    Bit position

---
+++ SumOfDigitsV2.py
@@ -0,0 +1,10 @@
+#INPUT SAMPLE: The first argument will be a path to a filename containing positive integers, one per line.
+
+def fileReader(path):
+    with open(path) as file:
+        for line in file:
+            line = line.strip()
+            list_int = map(int,line)
+            print reduce(lambda x,y: x + y,list_int)
+
+fileReader("/Users/Dias/Desktop/test.txt")
