f5efe13b554856cf9f05349acec81a6e08960757
PedroSDD <psdd91@gmail.com>
2017-10-25 10:12:17 +0100

    CreditCardValidator

--- BinToDec.py
+++ BinToDec.py
@@ -7,14 +7,23 @@ import math
 
 def binToDec(binary_number):
 	binary_number_list = (map(int, str(binary_number)))
-	decimal = 0
+	# decimal = 0
 
-	for i, item in enumerate(binary_number_list):
-		if item == 1:
-			print ((len(binary_number_list)-i)-1)
-			decimal += math.pow(2,((len(binary_number_list)-i)-1))
-	return decimal
+	# for i, item in enumerate(binary_number_list):
+	# 	if item == 1:
+	# 		print ((len(binary_number_list)-i)-1)
+	# 		decimal += math.pow(2,((len(binary_number_list)-i)-1))
+	# return decimal
 
+
+	print sum(map(lambda x, y: x, y=len(binary_number_list) if x != 1 y+=1 else (math.pow(2,(len(binary_number_list))-1)) y-=1 , binary_number_list))
+
+
+
+	
+
+
+	#return map(lambda x: x if x < len(population) else (lenpopulation)-x)*-1, indexes)
 
 def main():
     binToDec(110)
